02faee3011449eea3c1b04af499fc5b675abb0cc
Cody Raskin <cody@antimatterhorn.com>
2020-03-11 16:58:49 -0700

    Init

---
+++ Assets/Captain.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Captain
+{
+    public Character character;
+    public Sprite portrait;
+    public string name;
+    public string title;
+
+    public int level;
+
+    public int accuracy;
+    public int inspiration;
+    public int initiative;
+    public int luck;
+
+    public Captain (Character baseCharacter)
+    {
+        character = baseCharacter;
+        name = character.name;
+        portrait = character.portrait;
+        accuracy = character.baseAccuracy;
+        inspiration = character.baseInspiration;
+        initiative = character.baseInitiative;
+        luck = character.baseLuck;
+        level = 1;
+    }
+
+    public void levelUp()
+    {
+        level++;
+        int traits = 2;
+
+
+    }
+
+    public void levelUp(int traits)
+    {
+        level++;
+
+    }
+}
---
+++ Assets/Character.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Character", menuName = "Character")]
+public class Character : ScriptableObject
+{
+    public new string name;
+    public Sprite portrait;
+
+    public int baseAccuracy;
+    public int baseInspiration;
+    public int baseInitiative;
+    public int baseLuck;
+}
