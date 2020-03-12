7f3570ab7aae30454d67b7841088a0345d4c7926
Cody Raskin <cody@antimatterhorn.com>
2020-03-12 09:15:46 -0700

    Custom previewer for characters

---
+++ Assets/Editor/CharacterInspector.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomPreview(typeof(Character))]
+public class CharacterInspector : ObjectPreview
+{
+    public override bool HasPreviewGUI()
+    {
+        return true;
+    }
+
+    public override void OnPreviewGUI(Rect r, GUIStyle background)
+    {
+        
+        base.OnPreviewGUI(r, background);
+        Character character = (Character)target;
+        Sprite portrait = character.portrait;
+        DrawOnGUISprite(portrait);
+
+    }
+
+    void DrawOnGUISprite(Sprite aSprite)
+    {
+        Rect c = aSprite.rect;
+        float spriteW = c.width;
+        float spriteH = c.height;
+        Rect rect = GUILayoutUtility.GetRect(spriteW, spriteH);
+        if (Event.current.type == EventType.Repaint)
+        {
+            var tex = aSprite.texture;
+            c.xMin /= tex.width;
+            c.xMax /= tex.width;
+            c.yMin /= tex.height;
+            c.yMax /= tex.height;
+            GUI.DrawTextureWithTexCoords(rect, tex, c);
+        }
+    }
+
+    Texture2D GenerateTextureFromSprite(Sprite aSprite)
+    {
+        var rect = aSprite.rect;
+        var tex = new Texture2D((int)rect.width, (int)rect.height);
+        var data = aSprite.texture.GetPixels((int)rect.x, (int)rect.y, (int)rect.width, (int)rect.height);
+        tex.SetPixels(data);
+        tex.Apply(true);
+        return tex;
+    }
+}
